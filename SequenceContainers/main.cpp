642f85f68585f9ae3b2ccc3ac00f4a73706f54c2
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-10-01 21:42:19 +0300

    vector in progress

--- SequenceContainers/main.cpp
+++ SequenceContainers/main.cpp
@@ -1,19 +1,45 @@
 ﻿#include <iostream>
 #include <array>
+#include <vector>
 using std::cin;
 using std::cout;
 using std::endl;
 
 #define tab "\t"
+#define delimeter "\n----------------------------\n"
 
+template<typename T> void vector_info(const std::vector<T>& vec);
+
+//#define FACTORIAL
 //#define STL_ARRAY
-#define FACTORIAL
+#define STL_VECTOR
 
-unsigned long long int Factorial(int n);
+#ifdef FACTORIAL
+unsigned long long int Factorial(int n)
+{
+	if (n < 0) throw std::invalid_argument("Фактоиал не определен для отрицательных чисел");
+	if (n > 20) throw std::invalid_argument("Арифметическое переполнение");
+	if (n == 0 || n == 1) return 1;
+	return (unsigned long long int)n * Factorial(n - 1);
+}
+#endif // FACTORIAL
 
 int main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef FACTORIAL
+	int n;
+	cout << "n = "; cin >> n;
+	try
+	{
+		cout << n << "! = " << Factorial(n) << endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
+#endif // FACTORIAL
 
 #ifdef STL_ARRAY
 	//throw;
@@ -34,26 +60,36 @@ int main()
 	}*/
 #endif // STL_ARRAY
 
-#ifdef FACTORIAL
-	int n;
-	cout << "n = "; cin >> n;
-	try
-	{
-		cout << n << "! = " << Factorial(n) << endl;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << endl;
-	}
-#endif // FACTORIAL
+#ifdef STL_VECTOR
+	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i = 0; i < vec.size(); ++i) cout << vec[i] << tab; cout << endl;
+	vector_info(vec);
+	vec.push_back(55);
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) cout << *it << tab; cout << endl;
+	for (std::vector<int>::reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it) cout << *it << tab; cout << endl;
+	vector_info(vec);
+	//vec.shrink_to_fit();
+	//vec.reserve(1600);
+	vec.resize(8);
+	for (int i : vec) cout << i << tab; cout << endl;
+	vector_info(vec);
+	cout << vec.front() << endl;
+	cout << vec.back() << endl;
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	vec.insert(vec.begin() + index, value);
+	for (int i : vec) cout << i << tab; cout << endl;
+#endif // STL_VECTOR
 
 	return 0;
 }
 
-unsigned long long int Factorial(int n)
+template<typename T> void vector_info(const std::vector<T>& vec)
 {
-	if (n < 0) throw std::invalid_argument("Фактоиал не определен для отрицательных чисел");
-	if (n > 20) throw std::invalid_argument("Арифметическое переполнение");
-	if (n == 0 || n == 1) return 1;
-	return (unsigned long long int)n * Factorial(n - 1);
+	cout << "Size: " << vec.size() << endl;
+	cout << "MaxSize: " << vec.max_size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl;
+	cout << delimeter << endl;
 }
