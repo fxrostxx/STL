918e0379f24d15911e5f3a50f3d069badf4fbd1c
fxrostxx <fxrostxx@FXROSTXX-PC>
2025-10-03 18:11:41 +0300

    vector, list, forward_list insert and erase DONE

--- SequenceContainers/main.cpp
+++ SequenceContainers/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <array>
 #include <vector>
+#include <list>
+#include <forward_list>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -12,7 +14,9 @@ template<typename T> void vector_info(const std::vector<T>& vec);
 
 //#define FACTORIAL
 //#define STL_ARRAY
-#define STL_VECTOR
+//#define STL_VECTOR
+//#define STL_LIST
+#define STL_FORWARD_LIST
 
 #ifdef FACTORIAL
 unsigned long long int Factorial(int n)
@@ -64,24 +68,68 @@ int main()
 	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
 	for (int i = 0; i < vec.size(); ++i) cout << vec[i] << tab; cout << endl;
 	vector_info(vec);
+
 	vec.push_back(55);
 	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) cout << *it << tab; cout << endl;
 	for (std::vector<int>::reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it) cout << *it << tab; cout << endl;
 	vector_info(vec);
+
 	//vec.shrink_to_fit();
 	//vec.reserve(1600);
 	vec.resize(8);
 	for (int i : vec) cout << i << tab; cout << endl;
 	vector_info(vec);
+
 	cout << vec.front() << endl;
 	cout << vec.back() << endl;
+
 	int index;
 	int value;
 	cout << "Введите индекс добавляемого элемента: "; cin >> index;
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	vec.insert(vec.begin() + index, value);
 	for (int i : vec) cout << i << tab; cout << endl;
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	vec.erase(vec.begin() + index);
+	for (int i : vec) cout << i << tab; cout << endl;
 #endif // STL_VECTOR
+
+#ifdef STL_LIST
+	std::list<int> list1 = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i : list1) cout << i << tab; cout << endl;
+	int index, value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	auto it = list1.begin();
+	for (int i = 0; i < index; ++i) ++it;
+	list1.insert(it, value);
+	for (int i : list1) cout << i << tab; cout << endl;
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	it = list1.begin();
+	for (int i = 0; i < index; ++i) ++it;
+	list1.erase(it);
+	for (int i : list1) cout << i << tab; cout << endl;
+#endif // STL_LIST
+
+#ifdef STL_FORWARD_LIST
+	std::forward_list<int> list1 = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i : list1) cout << i << tab; cout << endl;
+	int index, value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	auto it = list1.begin();
+	for (int i = 0; i < index - 1; ++i) ++it;
+	list1.insert_after(it, value);
+	for (int i : list1) cout << i << tab; cout << endl;
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	it = list1.begin();
+	for (int i = 0; i < index - 1; ++i) ++it;
+	list1.erase_after(it);
+	for (int i : list1) cout << i << tab; cout << endl;
+#endif // STL_FORWARD_LIST
 
 	return 0;
 }
